57e5317c11ff96b5cf782b1bd92aa73b6c1af3e2
Barstok <mrbarstok@gmail.com>
2023-03-13 15:29:02 +0100

    init

---
+++ lab1/source/MyWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public string? videoFile {get; set;}
+    public string? audioFile {get; set;}
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet(string videoFile, string audioFile)
+    {
+        this.videoFile = videoFile;
+        this.audioFile = audioFile;
+        Console.WriteLine(videoFile);
+        Console.WriteLine(audioFile);
+    }
+}
