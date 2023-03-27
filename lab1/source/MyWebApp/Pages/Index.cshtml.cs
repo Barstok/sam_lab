54bc0502cfa9c8f493341c91219322db41979446
Barstok <mrbarstok@gmail.com>
2023-03-27 14:40:56 +0200

    Dokonczenie pierwszego zadania

--- lab1/source/MyWebApp/Pages/Index.cshtml.cs
+++ lab1/source/MyWebApp/Pages/Index.cshtml.cs
@@ -7,7 +7,9 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    [ViewData]
     public string? videoFile {get; set;}
+    [ViewData]
     public string? audioFile {get; set;}
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -19,6 +21,8 @@ public class IndexModel : PageModel
     {
         this.videoFile = videoFile;
         this.audioFile = audioFile;
+        ViewData["videoFile"] = this.videoFile;
+        ViewData["audioFile"] = this.audioFile;
         Console.WriteLine(videoFile);
         Console.WriteLine(audioFile);
     }
