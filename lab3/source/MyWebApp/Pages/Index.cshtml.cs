f184555073970a5906929b3e820df55f74b1326f
Barstok <mrbarstok@gmail.com>
2023-04-17 14:35:11 +0200

    lab3 folder init

---
+++ lab3/source/MyWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    [ViewData]
+    public string? videoFile {get; set;}
+    [ViewData]
+    public string? audioFile {get; set;}
+    [ViewData]
+    public string? imgFile {get; set;}
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet(string videoFile, string audioFile, string imgFile)
+    {
+        this.videoFile = videoFile;
+        this.audioFile = audioFile;
+        this.imgFile = imgFile;
+        ViewData["videoFile"] = this.videoFile;
+        ViewData["audioFile"] = this.audioFile;
+        ViewData["imgFile"] = this.imgFile;
+    }
+}
